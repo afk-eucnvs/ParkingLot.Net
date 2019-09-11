d024abcf7ceda7c4e41d81a0e7c896222417edd7
Andreas Fyrstelin Kristiansen <afkpost@gmail.com>
2019-09-11 10:10:23 +0200

    Added test

---
+++ ParkingLot.Tests/FotexPriceStrategyTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ParkingLot.Fotex;
+using Xunit;
+
+namespace ParkingLot.Tests
+{
+    public class FotexPriceStrategyTests
+    {
+        readonly IPriceStrategy strategy = new FotexPriceStrategy();
+
+        [Fact]
+        public void PriceFor6minutes()
+        {
+            Assert.Equal(15, strategy.CalcalatePrice(TimeSpan.FromMinutes(6)));
+        }
+    }
+}
